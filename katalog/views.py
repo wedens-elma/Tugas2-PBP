c061f38ec99aedcc9c230f0b033b638b0ed8509c
wedens.elma <wedens.elma@ui.ac.id>
2022-09-14 15:01:04 +0700

    Create a function that returns the data into HTML

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+
+from katalog.models import CatalogItem
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_item_katalog = CatalogItem.objects.all()
+context = {
+    'list_item': data_item_katalog,
+    'name': 'Wedens Elma Malau',
+    'npm' : '2106751165'
+}

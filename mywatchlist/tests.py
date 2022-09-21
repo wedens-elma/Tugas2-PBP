1d78a942625ac908ea3814904b9098e98eaca52b
wedens.elma <wedens.elma@ui.ac.id>
2022-09-21 22:27:59 +0700

    Add test cases

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class MyWatchlistTestCase(TestCase):
+    def test_base(self):
+        response = Client().get('/mywatchlist/')
+        self.assertEqual(response.status_code,200)
+
+    def test_html(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_xml(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def test_json(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
